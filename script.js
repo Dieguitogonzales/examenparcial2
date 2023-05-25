fe80dda252460a6253b1e8313030c00e4a18b6e1
Dieguitogonzales <132518146+Dieguitogonzales@users.noreply.github.com>
2023-05-24 22:08:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+let uno = true;
+let dos = "2";
+let tres = 3;
+let cuatro = null;
+let cinco;
+let seis = {numero: 6};
+let siete = [7];
+console.log(typeof true);
+console.log(typeof "2");
+console.log(typeof 3); 
+console.log(typeof null);
+console.log(typeof "nombre")
+console.log(typeof {numero: 6});
+console.log(typeof [7]);
+
+let = nombreProducto="calculadora", precio = 20, direccionEnvio ="santa cruz", país="bolivia" , costoEnvio=20; 
+
+let libreria1 = "React es una biblioteca Javascript de código abierto creada por Facebook.";
+
+let libreria2 = "React es una biblioteca Javascript de código abierto creada por Facebook.";
+console.log(libreria2.length);
+
+let string = "React es una biblioteca";
+console.log(string.replace("React es una biblioteca", "ReactNative es un framework")); 
+
+let libreria3 = "REACT es una BIBLIOTECA JavaScript de CÓDIGO ABIERTO CREADA por FACEBOOK";
+console.log(libreria3.concat("REACT es una BIBLIOTECA JavaScript de CÓDIGO ABIERTO CREADA por FACEBOOK")); 
+
+let libreria4 = "REACT es una BIBLIOTECA JavaScript de CÓDIGO ABIERTO CREADA por FACEBOOK"
+console.log(libreria4.toLowerCase());
+
+let string2 = "TOYOTA, SUZUKI, BMW, FORD, KIA, ISUZU, AUDI";
+console.log(string2.split(","));
+
+let string3 = "programacion";
+console.log(string.charCodeAt(1)); 
+
+let persona = (" HOLA MUNDO ");
+console.log(persona.repeat(4));
+
+console.log(`Hola estudio "Sistema" y estoy llevando la materia de:
+PROGRAMACION 1
+Turno: Noche
+En el grupo 2`);
+
+let random = Math.random();
+let numero = random * 24
+let valorFinal = numero + 50
+console.log(Math.floor(valorFinal));
+
+let libreria = "React es una biblioteca Javascript de código abierto creada por Facebook."
+let firstLetter1 = libreria[8];
+console.log(firstLetter1);
+
+let js = 'JavaScript' 
+let firstLetter = js[2];
+console.error(firstLetter);
+
+/*9.8     -> Numero entero
+-5      -> Numero Absoluto
+62      -> Cadena de texto
+"28"    -> Numero entero
+"15.65" -> Numero decimal*/
+let num1 = 9.8;
+let numInt = parseInt(num1);
+
+console.log(numInt);
+ let num2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
